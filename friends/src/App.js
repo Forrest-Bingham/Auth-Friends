c1017472b3b00e991d7b9d0b65e579eb71272f18
Forrest Bingham <bingham_5@live.com>
2019-12-19 11:23:47 -0700

    Added new Title

--- friends/src/App.js
+++ friends/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <h1> Friends List!</h1>
+        <h1> Friends List!!</h1>
         <ul>
           <li>
             <Link to="/login">Login</Link>
